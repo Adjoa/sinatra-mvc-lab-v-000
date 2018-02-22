1fc11e73c8555b4a8c3b9799ad8d03f7d3cc8b73
Adjoa Darien <adjoa.darien@gmail.com>
2018-02-22 15:45:34 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ class App < Sinatra::Base
   end
 
   post '/piglatinize' do
-    @pig_latin = PigLatinizer.new.piglatinize(params[:user_phrase])
+    @pig_latin = PigLatinizer.new.to_pig_latin(params[:user_phrase])
     binding.pry
     erb :results
   end
